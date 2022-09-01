1fc16a3ae37ed16f3cd843e0a9dd106273235225
giuliamaia <gmaiaserafim@gmail.com>
2022-08-31 22:36:05 -0300

    conexão com kafka

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.listen(port, () => {
  
     try {
         const resultado = await database.sync();
-        console.log(resultado);
+        // console.log(resultado);
 
         // const resultadoCreate = await User.create({
         //     nome: 'Lucas Meliante',
@@ -28,3 +28,41 @@ app.listen(port, () => {
         console.log(error);
     }
 })();
+
+const { Kafka, Partitioners } = require('kafkajs');
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092'],
+});
+
+const producer = kafka.producer({createPartitioner: Partitioners.DefaultPartitioner});
+const consumer = kafka.consumer({ groupId: 'test-group' });
+
+async function Consumer() {
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+  
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      })
+    },
+  });
+}
+
+async function Producer() {
+  await producer.connect();
+  await producer.send({
+    topic: 'test-topic',
+    messages: [
+      { value: 'Hello KafkaJS user!' },
+    ],
+  });
+  
+  await producer.disconnect();
+}
+
+Producer();
+Consumer();
