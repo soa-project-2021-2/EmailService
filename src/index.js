cb3259cfb90994e14c5f145e3752b8e90b8b7e8e
giuliamaia <gmaiaserafim@gmail.com>
2022-08-31 23:28:47 -0300

    teste heroku 2

--- src/index.js
+++ src/index.js
@@ -29,40 +29,40 @@ app.listen(port, () => {
     }
 })();
 
-const { Kafka, Partitioners } = require('kafkajs');
+// const { Kafka, Partitioners } = require('kafkajs');
 
-const kafka = new Kafka({
-  clientId: 'my-app',
-  brokers: ['localhost:9092'],
-});
+// const kafka = new Kafka({
+//   clientId: 'my-app',
+//   brokers: ['localhost:9092'],
+// });
 
-const producer = kafka.producer({createPartitioner: Partitioners.DefaultPartitioner});
-const consumer = kafka.consumer({ groupId: 'test-group' });
+// const producer = kafka.producer({createPartitioner: Partitioners.DefaultPartitioner});
+// const consumer = kafka.consumer({ groupId: 'test-group' });
 
-async function Consumer() {
-  await consumer.connect();
-  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+// async function Consumer() {
+//   await consumer.connect();
+//   await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
   
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        value: message.value.toString(),
-      })
-    },
-  });
-}
+//   await consumer.run({
+//     eachMessage: async ({ topic, partition, message }) => {
+//       console.log({
+//         value: message.value.toString(),
+//       })
+//     },
+//   });
+// }
 
-async function Producer() {
-  await producer.connect();
-  await producer.send({
-    topic: 'test-topic',
-    messages: [
-      { value: 'Hello KafkaJS user!' },
-    ],
-  });
+// async function Producer() {
+//   await producer.connect();
+//   await producer.send({
+//     topic: 'test-topic',
+//     messages: [
+//       { value: 'Hello KafkaJS user!' },
+//     ],
+//   });
   
-  await producer.disconnect();
-}
+//   await producer.disconnect();
+// }
 
 // Producer();
 // Consumer();
